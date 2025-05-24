2e0efcace686099d52f06f8400282790df423a06
Andreas Yuji Fujiki <andreaspinheiroprincipal@gmail.com>
2025-05-24 04:14:55 -0300

    new(typescript): Add numeric sets classification script.

--- 01-numeric-sets/numericSets.ts
+++ 01-numeric-sets/numericSets.ts
@@ -78,8 +78,8 @@ async function main() {
                 '√13', '√14', '√15', '√17', '√18', '√19', '√20', '√21', '√22', '√23', 
                 '√24', '√26', '√27', '√28', '√29', '√30', '√31', '√33', '√34', '√35', 
                 '√37', '√38', '√39', '√40', 'log₂3', 'log₁₀2', 'logₑ5', 'log₃7', 'log₄9', 
-                '2^√2', 'e^π', 'π^e', 'e^√2', '3^√5', 'sin(1)', 'cos(1)', 'tan(1)', 
-                'arcsin(1/2)', 'arccos(1/3)', 'γ', 'ζ(3)', 'G', 'K'
+                '2^√2', 'e^π', 'π^e', 'e^√2', '3^√5', 'sin(1)', 'cos', 'tan', 
+                'arcsin', 'arccos(1/3)', 'γ', 'Y', 'y', 'ζ', 'G', 'K'
             ];
 
             const isIrrational = irrationalNumbersList.some(item => item === normalizedValue);
@@ -98,7 +98,7 @@ async function main() {
                 || isIrrational;
         
         // saving valid sets for user's input number
-            if(isNaturalExcludingZero){ // Natural, Integer, Real
+            if(isNaturalExcludingZero){
                 sets.push('Natural excluding zero');
             };
 
@@ -135,12 +135,11 @@ async function main() {
         
         const formattedMessage = 
             sets.length === 0 
-                ? `${stringValue} is not a valid number!` 
+                ? `"${stringValue}" is not a valid number!` 
                 : `Value ${stringValue} contains the following sets: ${sets.join(', ')}`;
 
         console.log(formattedMessage);
 
         rl.close();
 }
-
 main();
